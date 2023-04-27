fe301caabb30af67ce2f437d62fbff726762e60e
dan-zayas <dmzayasplus@gmail.com>
2023-04-27 18:52:41 -0500

    debugging error logs for payment processing

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,23 +1,38 @@
-require("dotenv").config();
+// Netlify Function (stripe-handler.js)
+import stripePackage from 'stripe';
 
-const apiKey = `${import.meta.env.STRIPE_SECRET_KEY}`;
-
-console.log(apiKey);
-
-const stripe = require("stripe")(apiKey);
+const apiKey = process.env.STRIPE_SECRET_KEY;
+const stripe = stripePackage(apiKey);
 
 export async function handler(event) {
   try {
+    if (!event.body) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Invalid request body' }),
+      };
+    }
+
     const { amount } = JSON.parse(event.body);
+
+    if (!amount) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Missing amount' }),
+      };
+    }
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-      currency: "usd",
-      payment_method_types: ["card"],
+      currency: 'usd',
+      payment_method_types: ['card'],
     });
 
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Update this to your specific domain if needed
+      },
       body: JSON.stringify({ paymentIntent }),
     };
   } catch (error) {
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: 
-    [
-      react(),
-      svgr()
-    ],
-})
+  plugins: [react(), svgr()],
+  define: {
+    'process.env': process.env,
+  },
+});
